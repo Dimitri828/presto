615f08e3abd59bc3cfc352de834926611ad98ed9
Dimitri <dimitrifortini91@gmail.com>
2026-05-05 10:19:33 +0200

    finito pagina annunci

--- annunci.js
+++ annunci.js
@@ -16,127 +16,149 @@
 
 
 
-fetch("./annunci.json").then((response)=>response.json()).then((data)=>{
+fetch("./annunci.json").then((response) => response.json()).then((data) => {
 
-    data.sort((a,b)=>a.price-b.price);
+    data.sort((a, b) => a.price - b.price);
 
 
-    let radioWrapper=document.querySelector("#radioWrapper")
-    let cardWrapper=document.querySelector("#cardWrapper")
+    let radioWrapper = document.querySelector("#radioWrapper")
+    let cardWrapper = document.querySelector("#cardWrapper")
 
     function radioCreate() {
-        let categories=data.map((annuncio)=>annuncio.category)
-        
+        let categories = data.map((annuncio) => annuncio.category)
+
         // METODO CON FOR EACH...il !davanti unquie è un not, grazie a quello includes diventa not includes
         // let uniqueCategories=[]
         // categories.forEach((category)=>{
         //     if(!uniqueCategories.includes(category)){
         //         uniqueCategories.push(category)
         //     }
-            
+
         // })
         // Con METODO SET 
         // Set():classe che mi restituisce partendo da un array un nuovo oggetto di tipo Set il quale contiene solo valori univoci
         // Array.from():Mi permette di convertire un array-like in un array
-        let uniqueCategories= Array.from(new Set(categories))
-        
-        uniqueCategories.forEach((category)=>{
-            let div= document.createElement("div");
+        let uniqueCategories = Array.from(new Set(categories))
+
+        uniqueCategories.forEach((category) => {
+            let div = document.createElement("div");
             div.classList.add("form-check");
-            div.innerHTML=`
+            div.innerHTML = `
             <input class="form-check-input" type="radio" name="Categories" id="${category}">
             <label class="form-check-label" for="${category}">
             ${category}
             </label>
             `;
-            radioWrapper.appendChild(div)                    
+            radioWrapper.appendChild(div)
         })
     }
     radioCreate()
-    
-    
+
+
     function showCards(array) {
-        cardWrapper.innerHTML=""
-        array.forEach((annuncio)=>{
-            let div=document.createElement("div");
+        cardWrapper.innerHTML = ""
+        array.forEach((annuncio) => {
+            let div = document.createElement("div");
             div.classList.add("card-custom");
-            div.innerHTML=`<p class="h2" title="${annuncio.name}">${truncateWord(annuncio.name)}</p>
+            div.innerHTML = `<p class="h2" title="${annuncio.name}">${truncateWord(annuncio.name)}</p>
                     <p class="h4">${annuncio.category}</p>
                     <p class="lead">${annuncio.price} €</p>`
-                    
-                    cardWrapper.appendChild(div)
-                })
+
+            cardWrapper.appendChild(div)
+        })
     }
 
-    function truncateWord(string){
-        if (string.length >15) {
-            return string.split(" ")[0]+"...";
-        }else{
+    function truncateWord(string) {
+        if (string.length > 15) {
+            return string.split(" ")[0] + "...";
+        } else {
             return string
         }
     }
-    function filterByCategory(categoria) {
+    let radioButtons = document.querySelectorAll(".form-check-input")
+    function filterByCategory(array) {
         // In questa funzione devo ottenere un nuovo array partendo da data e gli elementi del nuovo array dovranno soddisfare per la quale la loro category,sia uguale alla categoria che stiamo passando alla funzione
-        if(categoria!= "All"){
-        let filtered= data.filter((annuncio)=> annuncio.category==categoria)
-        showCards(filtered)
-        }else{
-            showCards(data)
-        }
         
+        let categoria= Array.from(radioButtons).find((button)=>button.checked).id
+    //    Stesso metodo splittato in passaggi
+        // let arrayFromNodeList= Array.from(radioButtons)
+        // let button =arrayFromNodeList.find((button)=>button.checked)
+        // let categoria= button.id
+        
+
+        if (categoria != "All") {
+            let filtered = data.filter((annuncio) => annuncio.category == categoria)
+            
+            
+            return filtered
+        } else {
+            return array
+        }
+
     }
 
-    let radioButtons= document.querySelectorAll(".form-check-input")
-    radioButtons.forEach((button)=>{
-        button.addEventListener("click",()=>{
-            filterByCategory(button.id)
+    radioButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            setPriceInput(filterByCategory(data))
+               globalFilter()
+
         })
     })
     showCards(data)
 
-let priceInput=document.querySelector("#priceInput")
-let priceValue=document.querySelector("#priceValue")
+    let priceInput = document.querySelector("#priceInput")
+    let priceValue = document.querySelector("#priceValue")
 
-    function setPriceInput() {
+    function setPriceInput(array) {
         // dopo aver catturato l'input voglio settare come proprietà max dello stesso il valore più alto tra i price di ogni prodotto.Per farlo avrò quindi bisogno di un array che contenga solo i prezzi, a quel punto lo ordino in maniera crescente o decrescente e prendermi l'elemento con il valore più alto.
 
         // Il + davanti rende le stringhe numeri
 
-       let prices= data.map((annuncio)=>+annuncio.price)
-       prices.sort((a,b)=>a-b);
-       let maxPrice= Math.ceil(prices.pop());
-       priceInput.max=maxPrice
-       priceInput.value=maxPrice
-       priceValue.innerHTML=maxPrice
-        
-    }
-    
-    setPriceInput()
-    
-    
-    function filterByPrice(){
-        let filtered= data.filter((annuncio)=>+annuncio.price <= priceInput.value);
-        showCards(filtered)
-        priceValue.innerHTML=priceInput.value
-        
-        
-    }
-    priceInput.addEventListener("input",()=>{
-        
-        filterByPrice()
-    })
+        let prices = array.map((annuncio) => +annuncio.price)
+        prices.sort((a, b) => a - b);
+        let maxPrice = Math.ceil(prices.pop());
+        priceInput.max = maxPrice
+        priceInput.value = maxPrice
+        priceValue.innerHTML = maxPrice
 
-    let wordInput=document.querySelector("#wordInput")
-
-    function filterByWord(string) {
-        let filtered=data.filter((annuncio)=>annuncio.name.toLowerCase().includes(string.toLowerCase()));
-        showCards(filtered)
-        
-        
     }
-    wordInput.addEventListener("input",()=>{
-        filterByWord(wordInput.value)
+
+    setPriceInput(filterByCategory(data))
+
+
+    function filterByPrice(array) {
+        let filtered = array.filter((annuncio) => +annuncio.price <= priceInput.value);
+        
+        priceValue.innerHTML = priceInput.value
+        return filtered
+
+    }
+    priceInput.addEventListener("input", () => {
+
+            globalFilter()
 
     })
-    
+
+    let wordInput = document.querySelector("#wordInput")
+
+    function filterByWord(array) {
+        let filtered = array.filter((annuncio) => annuncio.name.toLowerCase().includes(wordInput.value.toLowerCase()));
+        return filtered
+
+
+    }
+    wordInput.addEventListener("input", () => {
+            globalFilter()
+
+        // Quello di cui abbiamo bisogno è che ad ogni evento scattino tutte e tre le funzioni concatenandosi una all'altra e non partendo da data
+    })
+    function globalFilter(){
+        let filteredByCategory=filterByCategory(data);   //array filtrato per categoria
+        let filteredByPrice=filterByPrice(filteredByCategory); //prezzo
+        let filteredByWord=filterByWord(filteredByPrice) // array filtrato per categoria prezzo e parola
+        showCards(filteredByWord)
+    }
+
+
+    // globalFilter()
 })
